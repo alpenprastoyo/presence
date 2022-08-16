86b259021cfef3390c3fb23269fcf92c0bb2cd9d
Alpen Prastoyo <alpenprastoyo@gmail.com>
2022-08-16 15:39:50 +0000

    a

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BooksSeeder::class);
+        // \App\Models\User::factory(10)->create();
     }
 }
